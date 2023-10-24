5892296b59a9a7de41afae332858f9da971a1ff1
archalke <avinashrchalke@gmail.com>
2023-10-24 15:37:57 -0500

    new cat added

--- js/main.js
+++ js/main.js
@@ -220,6 +220,23 @@
                 document.getElementById('products-placeholder').innerHTML = data;
             });
 
+    // search a product
+//    document.getElementById('searchInput').addEventListener('input', function (event) {
+//        const searchTerm = event.target.value.toLowerCase();
+//        const listItems = document.querySelectorAll('#itemList li');
+//
+//        listItems.forEach(function (item) {
+//            const itemText = item.textContent.toLowerCase();
+//
+//            if (itemText.includes(searchTerm)) {
+//                item.style.display = 'list-item';
+//            } else {
+//                item.style.display = 'none';
+//            }
+//        });
+//    });
+
+
     /*-------------------
 		Quantity change
 	--------------------- */
